e99224915a5a190ecc153ab22e0e601260ce8dfb
Chris Lee <iam@chrisjlee.net>
2014-06-22 01:33:16 -0500

    Correct js error

--- app/scripts/models/business.js
+++ app/scripts/models/business.js
@@ -1,9 +1,9 @@
-Yrf.Business = DS.Model.extend{[
-    "id": DS.attr('string'),
-    "rating_img_url": DS.attr('string'),
-    "country_code": DS.attr('string'),
-    // "is_closed": DS.attr('boolean'),
-    "address1": DS.attr('string'),
-    "address2": DS.attr('string'),
-    "phone": DS.attr('string')
- ]};
+Yrf.Business = DS.Model.extend({
+    id: DS.attr('string'),
+    rating_img_url: DS.attr('string'),
+    country_code: DS.attr('string'),
+    is_closed: DS.attr('boolean'),
+    address1: DS.attr('string'),
+    address2: DS.attr('string'),
+    phone: DS.attr('string')
+ });
