fe880a1326557a2dfcc1bf1eb2a764a926eadcf9
Chris Lee <iam@chrisjlee.net>
2014-06-22 04:42:10 -0500

    Modify model

--- app/scripts/models/business.js
+++ app/scripts/models/business.js
@@ -2,7 +2,7 @@ Yrf.Business = DS.Model.extend({
     id: DS.attr('string'),
     rating_img_url: DS.attr('string'),
     country_code: DS.attr('string'),
-    is_closed: DS.attr('boolean'),
+    is_closed: DS.attr(),
     address1: DS.attr('string'),
     address2: DS.attr('string'),
     phone: DS.attr('string')
