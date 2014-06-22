d55174a396336d777fb3a3e06064bf08e2ec80f9
chris <chris@wl600449.(none)>
2014-06-21 21:32:01 -0500

    Create simple router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,3 +1,12 @@
-Prf.Router.map(function () {
-  // Add your routes here
+Yrf.Router.map(function () {
+    this.route('/');
 });
+
+
+Yrf.IndexRoute = Ember.Route.extend({
+  renderTemplate: function () {
+    this.render('index');
+  }
+});
+
+
